df931fd38c837073171a61f1a8e07d738c2b1929
Alec <aweber@arrayschool.com>
2016-11-15 17:01:20 -0700

    end of the day

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+  $('#slide').click(function(){
+    $('#jquery').slideToggle(2000);
+  });
+  $('#fadee').click(function(){
+    $('#jquery').fadeToggle(2000);
+  });
+  $('#jquery').click(function(){
+    $('div, ul li:nth-child(2)').fadeTo('slow', 0.5);
+  });
 });
--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$("#slide").click(function(){$("#jquery").slideToggle(2e3)}),$("#fadee").click(function(){$("#jquery").fadeToggle(2e3)}),$("#jquery").click(function(){$("div, ul li:nth-child(2)").fadeTo("slow",.5)})});
